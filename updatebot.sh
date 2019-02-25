0aa19aa48d03e4587788196a8f032a6586fbbcf6
ryandawsonuk <ryandawson@cantab.net>
2019-02-25 17:33:51 +0000

    correct command

--- updatebot.sh
+++ updatebot.sh
@@ -7,5 +7,4 @@ set -e
 #the default file is .updatebot.yml but that would be a hidden file
 export UPDATEBOT_CONFIG_FILE=updatebot.yml
 
-
-java -jar updatebot-1.1-SNAPSHOT.jar push-version --kind maven maven org.activiti.build:activiti-parent 7.0.TESTPR73
+java -jar updatebot-1.1-SNAPSHOT.jar push-version --kind maven org.activiti.build:activiti-parent 7.0.TESTPR73
